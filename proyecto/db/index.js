09d89a4ee5214f13765c749f4649fc46cecb35c8
aldidjamil <alditamadjamil@gmail.com>
2023-02-18 12:48:17 +0100

    12:48

--- proyecto/db/index.js
+++ proyecto/db/index.js
@@ -6,9 +6,10 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/@latest";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/proyecto";
 
 mongoose
+  .set('strictQuery', false)
   .connect(MONGO_URI)
   .then((x) => {
     const databaseName = x.connections[0].name;
