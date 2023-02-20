fb7380b9b7c3a022c945b90bb81389a171e425af
aldidjamil <alditamadjamil@gmail.com>
2023-02-20 16:32:58 +0100

    lunes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -12,17 +12,51 @@ router.get("/", (req, res, next) => {
     res.render("index");
 });
 
-// router.get("/list-theaters", (req, res, next) => {
+router.get("/250-movies", (req, res, next) => {
 
 
-//     // res.send('Hola'))
-//     moviesApi
-//         .getAllMovies()
-//         // .then(response => res.render("movies/list-thaeaters", { movie: response.data }))
-//         .then(response => console.log(response))
+    // res.send('Hola'))
+    moviesApi
+        .getAllMovies()
+        .then(movies => {
+            // res.json(movies)
+            res.render("movies/movieRender", { movies })
+        })
+        // .then(response => console.log(response.data))
+        // .then(response => console.log(response))
+        .catch(err => console.log(err))
+})
 
-//         // .then(response => console.log(response))
-//         .catch(err => console.log(err))
-// })
+router.get("/inTheaters", (req, res, next) => {
+
+    moviesApi
+        .getInTheaters()
+        .then(movies => {
+            res.render("movies/movieRender", { movies })
+        })
+        .catch(err => next(err))
+})
+
+router.get("/mostPopular", (req, res, next) => {
+    // res.send('entro?')
+
+    moviesApi
+        .getMostPopular()
+        .then(movies => {
+            // res.json(movies)
+            res.render("movies/movieRender", { movies })
+        })
+
+})
+
+router.get("/comingSoon", (req, res, next) => {
+
+    moviesApi
+        .getComingSoon()
+        .then(movies => {
+            // res.json(movies)
+            res.render("movies/movieRender", { movies })
+        })
+})
 
 module.exports = router;
